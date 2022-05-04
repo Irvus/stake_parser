91a2c6305c4f918e97401d89fbe258f195f2752c
irvus <iatarasenko@edu.hse.ru>
2022-05-04 23:54:32 +0300

    now can see amount and cost of tokens by prometheus

--- exporter.py
+++ exporter.py
@@ -1,24 +1,23 @@
-from prometheus_client import start_http_server, Gauge, Summary
-import random
-import time
 import json
 import requests
-from pycoingecko import CoinGeckoAPI
+import time
 
+from prometheus_client import start_http_server, Info
+from pycoingecko import CoinGeckoAPI
 
 cg = CoinGeckoAPI()
 
-cerberus_link = "https://api.cerberus.zone:1317/staking/validators/cerberusvaloper1xjgspyv73d3k3ewygu0v2gcwwplwxkxg03reqy"
-# Create a metric to track time spent and requests made.
-bounded_tokens = Gauge('my_inprogress_requests', 'Description of gauge')
+cerberus_link = "https://api.cerberus.zone:1317/staking/validators" \
+                "/cerberusvaloper1xjgspyv73d3k3ewygu0v2gcwwplwxkxg03reqy"
+
+bounded_tokens = Info('my_inprogress_requests', 'here was Ilia')
 
 
-# Decorate function with metric.
-@bounded_tokens.track_inprogress()
 def process_request(t):
     parsed_json = requests.get(cerberus_link).json()
-    coin_cost = float(k.get('cerberus-2').get('usd'))
-    bounded_tokens.set_function(lambda: int(parsed_json.get('result').get('tokens')))
+    coin_cost = float(cg.get_price(ids='cerberus-2', vs_currencies='usd').get('cerberus-2').get('usd'))
+    bounded_tokens.info({'staked tokens': parsed_json.get('result').get('tokens'),
+                         'turnover': str(float(parsed_json.get('result').get('tokens')) * coin_cost)})
     time.sleep(t)
 
 
