f1a19f7a98e61ae364a676f569af11abed2e21e1
irvus <iatarasenko@edu.hse.ru>
2022-05-24 21:35:54 +0300

    now fixed some ways to parse data

--- exporter.py
+++ exporter.py
@@ -23,6 +23,12 @@ validators = [
      },
     {'name': 'desmos',
      'url': "https://api-desmos.cosmostation.io/v1/staking/validator/desmosvaloper1zngdx77g9ywnwmwpwvj9w2eqcs6fhw78gn02d8"
+     },
+    {'name': 'injective-protocol',
+     'url': "https://api-inj.cosmostation.io/v1/staking/validator/injvaloper1kpfxtqt5cmlew46dem32fqlk5g6k4wyueh4szu"
+     },
+    {'name': 'stargaze',
+     'url': "https://api-stargaze.cosmostation.io/v1/staking/validator/starsvaloper15cu52e36jth9gqr0muq755syvgyr96cr98a549"
      }
 ]
 
@@ -30,8 +36,17 @@ validators = [
 def process_request(link, net_name):
     parsed_json = requests.get(link).json()
     coin_cost = float(cg.get_price(ids=net_name, vs_currencies='usd').get(net_name).get('usd'))
-    bounded_tokens = {'net_name': net_name, 'staked tokens': str(float(parsed_json.get('result').get('tokens')) / 1000000),
-                      'turnover': str(float(parsed_json.get('result').get('tokens')) / 1000000 * coin_cost)}
+
+    try:
+        temp = str(float(parsed_json.get('result').get('tokens')) / 1000000)
+    except:
+        if (parsed_json.get('jailed') == True):
+            temp = str(float(parsed_json.get('tokens')) / 10e17)
+        else:
+            temp = str(float(parsed_json.get('tokens')) / 1000000)
+
+    bounded_tokens = {'net_name': net_name, 'staked tokens': temp,
+                      'turnover': str(float(temp) * coin_cost)}
 
     return bounded_tokens
 
